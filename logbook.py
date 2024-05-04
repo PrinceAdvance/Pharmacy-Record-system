c44eacbfb1f52555d276f934780a45f24cae186b
Prince Advance <princeadvanceagbenu@gmail.com>
2024-05-04 17:03:58 +0000

    files

--- logbook.py
+++ logbook.py
@@ -11,34 +11,34 @@ import os
 import openpyxl
 
 root = tk.Tk()
-root.title("Planned and Unplanned Jobs")
+root.title("Patient Recording System")
 
 
 
 # enter data code
 def enter_data():
-    firstname = name_of_engineer_combobox.get()
+    firstname = name_of_patient_combobox.get()
     surname = surname_combobox.get()
     date_id = date_id_Entry.get()
-    truck_id = truck_id_combobox.get()
-    workdone = workdone_Entry.get()
+    prescribed_drug = truck_id_combobox.get()
+    Doctor = Doctor_Entry.get()
 
     print("First name: ",firstname, "Surname: ", surname)
-    print("Equipment: ", truck_id)
+    print("Equipment: ", prescribed_drug)
     print("Date: ", date_id)
-    print("Workdone: ", workdone)
+    print("Doctor: ", Doctor)
 
     filepath = "C:\\Users\\Admin\\Desktop\\MyPython Projects\\Tkinter Projects\\logbook project\\logbook.py.xlsx"
 
     if not os.path.exists(filepath):
         workbook = openpyxl.Workbook()
         sheet = workbook.active
-        heading = ['first name','Surname','Date','Equipment','Workdone']
+        heading = ['first name','Surname','Date','Equipment','Doctor']
         sheet.append(heading)
         workbook.save(filepath)
     workbook = openpyxl.load_workbook(filepath)
     sheet = workbook.active
-    sheet.append([firstname, surname, truck_id,date_id, workdone]) 
+    sheet.append([firstname, surname, prescribed_drug,date_id, Doctor]) 
     workbook.save(filepath)   
 
 canvas = tk.Canvas(root, width = 300, height = 300)
@@ -51,8 +51,8 @@ frame.pack()
 frame1 = tk.LabelFrame(frame, text = "User Information", )
 frame1.grid(row = 0, column = 0, padx = 20, pady = 20)
 
-name_of_engineer = tk.Label(frame1, text = "First name")
-name_of_engineer.grid(row = 0, column = 0)
+name_of_patient = tk.Label(frame1, text = "First name")
+name_of_patient.grid(row = 0, column = 0)
 
 surname = tk.Label(frame1, text = "Surname")
 surname.grid(row = 0, column = 4)
@@ -60,23 +60,23 @@ surname.grid(row = 0, column = 4)
 date_id = tk.Label(frame1, text = "Date")
 date_id.grid(row = 0, column = 6)
   
-truck_id = tk.Label(frame1, text = "Truck ID")
-truck_id.grid(row = 2, column = 0)
+prescribed_drug = tk.Label(frame1, text = "Drug")
+prescribed_drug.grid(row = 2, column = 0)
 
-workdone = tk.Label(frame1, text = "Workdone")
-workdone.grid(row = 2, column = 4)
+Doctor = tk.Label(frame1, text = "Doctor")
+Doctor.grid(row = 2, column = 4)
 
-name_of_engineer_combobox = ttk.Combobox(frame1,values = ["perogozin"])
+name_of_patient_combobox = ttk.Combobox(frame1,values = ["perogozin"])
 surname_combobox = ttk.Combobox(frame1,values = [""])
 truck_id_combobox = ttk.Combobox(frame1,values = [""])
-workdone_Entry = tk.Entry(frame1)
+Doctor_Entry = tk.Entry(frame1)
 date_id_Entry = tk.Entry(frame1)
 
 
-name_of_engineer_combobox.grid(row = 1, column = 0)
+name_of_patient_combobox.grid(row = 1, column = 0)
 surname_combobox.grid(row = 1, column = 4)
 truck_id_combobox.grid(row = 3, column = 0)
-workdone_Entry.grid(row = 3, column = 4)
+Doctor_Entry.grid(row = 3, column = 4)
 date_id_Entry.grid(row = 1, column = 6)
 
 
